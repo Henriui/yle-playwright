56541f47936d3d193184dc20da11e15e8f4c208e
lassib <91030337+lassib@users.noreply.github.com>
2023-04-11 17:57:31 +0300

    kummeliTest fixed

--- tests/kummelitest.spec.ts
+++ tests/kummelitest.spec.ts
@@ -1,33 +1,28 @@
 import { test, expect } from '@playwright/test';
 
 test('check name and date of episode', async ({ page }) => {
-	await page.goto('https://areena.yle.fi/1-3339547');
+    await page.goto('https://areena.yle.fi/1-3339547');
 
-	await expect(page).toHaveTitle(/Kummeli/);
+    await expect(page).toHaveTitle(/Kummeli/);
 
-	const jaksot = await page.getByRole('list', {
-		name: 'Tuotantokaudet',
-		exact: true,
-	});
+    const jaksot = page.getByRole('list', {
+        name: 'Tuotantokaudet',
+        exact: true,
+    });
 
-	const season = await jaksot.getByRole('listitem').nth(2);
+    const season = jaksot.getByRole('listitem').nth(2);
 
-	await season.click();
+    await season.click();
 
-	expect(await season.getAttribute('aria-current')).toBe('true');
+    expect(await season.getAttribute('aria-current')).toBe('true');
 
-	const viidesJaksoNimi = page.getByRole('link', {
-		name: '5.',
-	});
+    const viidesJaksoNimi = page.getByRole('link', {
+        name: '5.',
+    });
 
-	console.log(await viidesJaksoNimi.innerText());
-
-	const viidesJaksoDate = page.getByLabel('CardLabels_genericLabel__eAJwW');
-
-	console.log(await viidesJaksoDate.innerText());
-
-	expect(await viidesJaksoNimi.innerText).toBe('Kausi 3. 5/6');
-	expect(await viidesJaksoDate.innerText).toBe('ti 8.3.2016');
+    expect(await viidesJaksoNimi.innerText()).toContain('5. Kummeli');
+        
+    expect(await page.locator('li.CardPage_listItem__JRVg1:nth-child(5) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)').innerText()).toContain('ti 8.3.2016');
 });
 
 /* 	await page.screenshot({
